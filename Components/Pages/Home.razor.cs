867f72e20241dc1b2b038c90bfb9227fea0bf258
Getten <Jeppe_marek@hotmail.com>
2026-02-05 00:29:45 +0100

    Now the homepage is divided into the types of categories

--- Components/Pages/Home.razor.cs
+++ Components/Pages/Home.razor.cs
@@ -7,16 +7,21 @@ public partial class Home : ComponentBase
 {
     private List<BreadcrumbItem> breadcrumbItems = new();
     private List<Model.Category> categories = new();
+    private List<Model.Category> parentCategories = new();
 
-    private async Task GetAllCategories()
-
+    private async Task GetAllParentCategories()
+    {
+        parentCategories = categories.Where(c => c.ParentCategoryId is null).ToList();
+    }
+    private async Task GetAllCategoriesAsync()
     {
         categories = await categoryService.GetAllCategoriesAsync();
     }
 
     protected override async Task OnInitializedAsync()
     {
-        await GetAllCategories();
+        await GetAllCategoriesAsync();
+        await GetAllParentCategories();
         breadcrumbItems = new List<BreadcrumbItem>
         {
             new("Home", null, true)
